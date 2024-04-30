70847a5dbe06f5e19e3ce6c8d6bea479af19ad7b
NateTheUnavailable <nate@stevenogardner.com>
2024-04-30 12:20:17 -0600

    work in progress not working yet

---
+++ week02/scripts/bom.js
@@ -0,0 +1,25 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('list');
+const li = document.createElement('li');
+
+button.addEventListener('click', function() {
+    if (input.value.trim() !== '') {
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.append(deleteButton);
+        list.append(li);
+
+        const deleteButton = document.createElement('button');
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        input.value = '';
+        input.focus();
+    }
+    else{
+        input.focus();
+    }
+});
